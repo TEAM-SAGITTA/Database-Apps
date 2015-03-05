6ca315ec8dda62a36f375caa9b4dd80ce5361510
MeLean <milen820430@gmail.com>
2015-03-05 23:22:16 +0200

    Button #6 made, some code must be tested

--- SagittaMain/MainWindow.xaml.cs
+++ SagittaMain/MainWindow.xaml.cs
@@ -1,19 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace SagittaMain
+﻿namespace SagittaMain
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -37,6 +38,17 @@ namespace SagittaMain
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("PDF Report Magic !!!\nDeveloper - Doncho Donchev");
+        }
+
+        private void LoadExpenseDataFromXML(object sender, RoutedEventArgs e)
+        {
+            string fileDirectoryName = @"\Files\expensesByVendorMonth.xml";
+            string currentDir = Directory.GetCurrentDirectory();
+            string binDir = System.IO.Directory.GetParent(currentDir).FullName;
+            string filePath = System.IO.Directory.GetParent(binDir).FullName + fileDirectoryName;
+            string fileText = System.IO.File.ReadAllText(filePath);
+            MessageBox.Show(fileText);
+            MessageBox.Show("Data loaded from XML file!");
         }
     }
 }
---
+++ TestingCSharpCode/TestXMLLoad.cs
@@ -0,0 +1,18 @@
+﻿namespace TestingCSharpCode
+{
+    using System.IO;
+
+    class TestXMLLoad
+    {
+        public static void Main()
+        {
+            string currentDir = Directory.GetCurrentDirectory();
+            var fileName = System.IO.Path.Combine(currentDir, @"..\..\expensesByVendorMonth.xml");
+  
+            string fileText = System.IO.File.ReadAllText(fileName);
+            System.Console.WriteLine(fileName);
+
+        }
+
+    }
+}
