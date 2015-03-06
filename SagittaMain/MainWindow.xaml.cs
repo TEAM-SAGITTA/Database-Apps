a16862643ada583ddd9a91da5d25495d0eff6309
d-donchev <ddonchev@gmail.com>
2015-03-06 15:02:12 +0200

    All Button events

--- SagittaMain/MainWindow.xaml.cs
+++ SagittaMain/MainWindow.xaml.cs
@@ -32,12 +32,12 @@
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Zip Import Magic !!!");
+            MessageBox.Show("Zip file to MS SQL Server Magic !!!");
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("PDF Report Magic !!!\nDeveloper - Doncho Donchev");
+            MessageBox.Show("MS SQL to PDF Magic !!!\nDeveloper -> ");
         }
 
         private void LoadExpenseDataFromXML(object sender, RoutedEventArgs e)
@@ -49,6 +49,41 @@
             string fileText = System.IO.File.ReadAllText(filePath);
             MessageBox.Show(fileText);
             MessageBox.Show("Data loaded from XML file!");
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("MS SQL to XML Magic !!!");
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("JSON to MongDB Magic !!!");
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("MS SQL to MySQL Magic !!!");
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("MySQL to Excel Magic !!!");
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Oralce to MS SQL Server Magic !!!");
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("SQLite to Excel Magic !!!");
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("MS SQL to JSON Magic !!!");
         }
     }
 }
