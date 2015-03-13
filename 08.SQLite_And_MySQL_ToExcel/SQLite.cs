93c467f4693236954e69357f5f77ace4ab5c40ee
d-donchev <ddonchev@gmail.com>
2015-03-14 00:26:01 +0200

    New SQLite Project

---
+++ 08.SQLite_And_MySQL_ToExcel/SQLite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace SQLite_And_MySQL_ToExcel
+{
+    public class SQLite
+    {
+    }
+}
