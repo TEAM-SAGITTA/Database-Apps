01b57b8131783212e9149cc6fb3a3a138b450e3c
presian <presiandanailov@gmail.com>
2015-03-15 17:04:36 +0200

    Last update

---
+++ 02.ZipReports_To_MSSQL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.ZipReports_To_MSSQL
+{
+    public class Class1
+    {
+    }
+}
