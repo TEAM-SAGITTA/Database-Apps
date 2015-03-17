5d9d3f34d1db3455fb6f9008c0bb685447f16736
d-donchev <ddonchev@gmail.com>
2015-03-17 23:38:47 +0200

    Create XML Document

---
+++ 04.XML_SalesReport/Output/XMLHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace _04.XML_SalesReport
+{
+    class XMLHelper
+    {
+        public void exportToXML()
+        {
+            
+            XmlDocument doc = new XmlDocument();
+            var sale = doc.CreateElement("sale");
+            var summary = doc.CreateElement("summary");
+            summary.InnerText = "date=\"20-Jul-2014\" total-sum=\"54.75\"";
+            summary.InnerText = "date=\"21-Jul-2014\" total-sum=\"40.35\"";
+            summary.InnerText = "date=\"21-Jul-2014\" total-sum=\"40.35\"";
+            sale.AppendChild(summary);
+            doc.DocumentElement.AppendChild(sale);
+            doc.Save("SalersReport.xml");
+        }
+        
+    }
+}
