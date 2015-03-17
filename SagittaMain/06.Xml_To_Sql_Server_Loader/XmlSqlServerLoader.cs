40d0ee84d8189b6b25331e8598f3c2b6f0673a1a
MeLean <milen820430@gmail.com>
2015-03-17 14:59:33 +0200

    Other project for task #6 created!

---
+++ SagittaMain/06.Xml_To_Sql_Server_Loader/XmlSqlServerLoader.cs
@@ -0,0 +1,88 @@
+﻿namespace _06.Xml_To_Sql_Server_Loader
+{
+    using System;
+    using System.Data.SqlClient;
+    using System.IO;
+    using System.Xml;
+
+    public class XmlSqlServerLoader
+    {
+
+        public static void DropCreateSqlServerTables(
+             SqlConnection connection,
+             string firstTableName,
+             string secondTableName,
+             SqlTransaction transaction)
+        {
+            string dropConstraintStr = string.Format("IF OBJECT_ID('{0}') IS NOT NULL ALTER TABLE {0} DROP CONSTRAINT FK_CompanyNameId;"
+                , secondTableName);
+
+            string createFirstTableStr = string.Format(
+                "IF OBJECT_ID('{0}') IS NOT NULL DROP TABLE {0} CREATE TABLE {0}" +
+                "(CompanyNameId  int IDENTITY(1,1) PRIMARY KEY, CompanyName NVARCHAR(MAX) NOT NULL)",
+                firstTableName);
+
+            string createSecondTableStr = string.Format(
+                "IF OBJECT_ID('{0}') IS NOT NULL DROP TABLE {0} CREATE TABLE {0}" +
+                "(Id  int IDENTITY(1,1) PRIMARY KEY, ExpenseMonth NVARCHAR(50) NOT NULL," +
+                "CompanyNameId int, Expenses NVARCHAR(20) NOT NULL," + //TODO make it money if you can
+                "CONSTRAINT FK_CompanyNameId FOREIGN KEY (CompanyNameId) REFERENCES {1}(CompanyNameId) ON DELETE SET NULL)",
+                secondTableName, firstTableName);
+
+            SqlCommand dropConstraint = new SqlCommand(dropConstraintStr, connection, transaction);
+            dropConstraint.ExecuteNonQuery();
+
+            SqlCommand createFirstTable = new SqlCommand(createFirstTableStr, connection, transaction);
+            createFirstTable.ExecuteNonQuery();
+
+            SqlCommand createSecondTable = new SqlCommand(createSecondTableStr, connection, transaction);
+            createSecondTable.ExecuteNonQuery();
+        }
+
+        public static void PolulateSqlTables(
+             SqlConnection connection,
+             string firstTableName,
+             string secondTableName,
+             SqlTransaction transaction)
+        {
+            string fileDirectoryName = @"\Files\expensesByVendorMonth.xml";
+            string currentDir = Directory.GetCurrentDirectory();
+            string binDir = Directory.GetParent(currentDir).FullName;
+            string filePath = Directory.GetParent(binDir).FullName + fileDirectoryName;
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filePath);
+            XmlElement root = xmlDoc.DocumentElement;
+            string RootPath = "/expenses-by-month/vendor";
+            string RootAtribute = "name";
+            string childName = "expenses";
+            string childAtribute = "month";
+            XmlNodeList rootNodes = root.SelectNodes(RootPath);
+            var nodeId = 1;
+
+            foreach (XmlNode vendor in rootNodes)
+            {
+                var RootAtributeValue = vendor.Attributes[RootAtribute].Value;
+                var insertStringCmd = string.Format("INSERT INTO {0} VALUES('{1}')", firstTableName, RootAtributeValue);
+                SqlCommand insertVendor = new SqlCommand(insertStringCmd, connection, transaction);
+                insertVendor.ExecuteNonQuery();
+                XmlNodeList childNodes = vendor.SelectNodes(childName);
+
+                for (int i = 0; i < childNodes.Count; i++)
+                {
+                    var expense = childNodes[i];
+                    var expenceMonth = expense.Attributes[childAtribute].Value;
+                    var expenceValue = float.Parse(expense.InnerText);
+
+                    string insertChildsStr = string.Format("INSERT INTO {0} VALUES({1},{2},'{3}')",
+                        secondTableName, expenceValue, (nodeId), expenceMonth);
+
+                    SqlCommand inserChilds = new SqlCommand(insertChildsStr, connection, transaction);
+                    inserChilds.ExecuteNonQuery();
+                }
+
+                nodeId++;
+            }
+        }
+    }
+}
--- SagittaMain/MainWindow.xaml.cs
+++ SagittaMain/MainWindow.xaml.cs
@@ -22,6 +22,8 @@
     using System.Diagnostics;
     using System.Reflection;
     using _02.ZipReports_To_MSSQL;
+    using _06.Xml_To_Sql_Server_Loader;
+ 
 
     public partial class MainWindow : Window
     {
@@ -51,20 +53,22 @@
         {
             //TODO change the connection string, Test DB is on my pc
             string DbConnectionString = "Server=.; Integrated security=SSPI; database=Test";
-            string firstTableName = "CompanyNames";
-            string secondTableName = "ExpencesByMonth";
 
             using (SqlConnection connection = new SqlConnection(DbConnectionString))
             {
                 connection.Open();
-
+                string firstTableName = "CompanyNames";
+                string secondTableName = "ExpencesByMonth";
+                
                 using (SqlTransaction transaction = connection.BeginTransaction())
                 {
                     try
-                    {
-                        DropCreateSqlServerTables(connection, firstTableName, secondTableName, transaction);
+                    {                       
+                        XmlSqlServerLoader.DropCreateSqlServerTables(
+                            connection, firstTableName, secondTableName, transaction);
 
-                        PolulateSqlTables(connection, firstTableName, secondTableName, transaction);
+                        XmlSqlServerLoader.PolulateSqlTables(
+                            connection, firstTableName, secondTableName, transaction);
 
                         transaction.Commit();
                         MessageBox.Show("Data loaded from XML file!");
@@ -135,83 +139,6 @@
         {
             var window = new Task5Window();
             window.Show();
-        }
-
-        private static void DropCreateSqlServerTables(
-      SqlConnection connection,
-      string firstTableName,
-      string secondTableName,
-      SqlTransaction transaction)
-        {
-            string dropConstraintStr = string.Format("IF OBJECT_ID('{0}') IS NOT NULL ALTER TABLE {0} DROP CONSTRAINT FK_CompanyNameId;"
-                , secondTableName);
-
-            string createFirstTableStr = string.Format(
-                "IF OBJECT_ID('{0}') IS NOT NULL DROP TABLE {0} CREATE TABLE {0}" +
-                "(CompanyNameId  int IDENTITY(1,1) PRIMARY KEY, CompanyName nvarchar(max) NOT NULL)",
-                firstTableName);
-
-            string createSecondTableStr = string.Format(
-                "IF OBJECT_ID('{0}') IS NOT NULL DROP TABLE {0} CREATE TABLE {0}" +
-                "(Id  int IDENTITY(1,1) PRIMARY KEY, ExpenseMonth nvarchar(50) NOT NULL," +
-                "CompanyNameId int, Expenses nvarchar(20) NOT NULL," + //TODO make it money if you can
-                "CONSTRAINT FK_CompanyNameId FOREIGN KEY (CompanyNameId) REFERENCES {1}(CompanyNameId) ON DELETE SET NULL)",
-                secondTableName, firstTableName);
-
-            SqlCommand dropConstraint = new SqlCommand(dropConstraintStr, connection, transaction);
-            dropConstraint.ExecuteNonQuery();
-
-            SqlCommand createFirstTable = new SqlCommand(createFirstTableStr, connection, transaction);
-            createFirstTable.ExecuteNonQuery();
-
-            SqlCommand createSecondTable = new SqlCommand(createSecondTableStr, connection, transaction);
-            createSecondTable.ExecuteNonQuery();
-        }
-
-        private static void PolulateSqlTables(
-            SqlConnection connection,
-            string firstTableName,
-            string secondTableName,
-            SqlTransaction transaction)
-        {
-            string fileDirectoryName = @"\Files\expensesByVendorMonth.xml";
-            string currentDir = Directory.GetCurrentDirectory();
-            string binDir = System.IO.Directory.GetParent(currentDir).FullName;
-            string filePath = System.IO.Directory.GetParent(binDir).FullName + fileDirectoryName;
-
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(filePath);
-            XmlElement root = xmlDoc.DocumentElement;
-            string RootPath = "/expenses-by-month/vendor";
-            string RootAtribute = "name";
-            string childName = "expenses";
-            string childAtribute = "month";
-            XmlNodeList rootNodes = root.SelectNodes(RootPath);
-            var nodeId = 1;
-
-            foreach (XmlNode vendor in rootNodes)
-            {
-                var RootAtributeValue = vendor.Attributes[RootAtribute].Value;
-                var insertStringCmd = string.Format("INSERT INTO {0} VALUES('{1}')", firstTableName, RootAtributeValue);
-                SqlCommand insertVendor = new SqlCommand(insertStringCmd, connection, transaction);
-                insertVendor.ExecuteNonQuery();
-                XmlNodeList childNodes = vendor.SelectNodes(childName);
-
-                for (int i = 0; i < childNodes.Count; i++)
-                {
-                    var expense = childNodes[i];
-                    var expenceMonth = expense.Attributes[childAtribute].Value;
-                    var expenceValue = float.Parse(expense.InnerText);
-
-                    string insertChildsStr = string.Format("INSERT INTO {0} VALUES({1},{2},'{3}')",
-                        secondTableName, expenceValue, (nodeId), expenceMonth);
-
-                    SqlCommand inserChilds = new SqlCommand(insertChildsStr, connection, transaction);
-                    inserChilds.ExecuteNonQuery();
-                }
-
-                nodeId++;
-            }
         }
     }
 }
