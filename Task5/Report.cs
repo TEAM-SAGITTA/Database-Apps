86df63fac37be2c43bc1eda449d3cb7cc9c3147b
presian <presiandanailov@gmail.com>
2015-03-20 16:34:09 +0200

    Task 5 all most done!

---
+++ Task5/Report.cs
@@ -0,0 +1,33 @@
+﻿namespace Task5
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    using Newtonsoft.Json;
+
+    class Report
+    {
+        [JsonIgnore]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "product-id")]
+        [BsonElement("product-id")]
+        public int  ProductId { get; set; }
+
+        [JsonProperty(PropertyName = "product-name")]
+        [BsonElement("product-name")]
+        public string ProductName { get; set; }
+
+        [JsonProperty(PropertyName = "vendor-name")]
+        [BsonElement("vendor-name")]
+        public string VendorName { get; set; }
+
+        [JsonProperty(PropertyName = "total-quantity-sold")]
+        [BsonElement("total-quantity-sold")]
+        public int TotalQuantitySold { get; set; }
+
+        [JsonProperty(PropertyName = "total-incomes")]
+        [BsonElement("total-incomes")]
+        public decimal TotalIncomes { get; set; }
+    }
+}
--- Task5/Utility.cs
+++ Task5/Utility.cs
@@ -1,4 +1,7 @@
-﻿namespace Task5
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Task5
 {
     using System;
     using System.Diagnostics;
@@ -19,7 +22,8 @@
         public static readonly DateTime CalendarEndDate = DateTime.Now;
         public static DateTime ReportStartDate = CalendarStartDate;
         public static DateTime ReportEndDate = DateTime.Now;
-        private static readonly BsonArray Reports = new BsonArray();
+        //        private static readonly BsonArray Reports = new BsonArray();
+        private static readonly List<Report> ReportList = new List<Report>();
 
 
         public static DateTime GetFirstReportDate()
@@ -31,7 +35,7 @@
         {
             try
             {
-                if (!Reports.Any())
+                if (!ReportList.Any())
                 {
                     GetReportsData();
                 }
@@ -41,10 +45,10 @@
                     Directory.CreateDirectory(Constants.ReportsFolder);
                 }
 
-                string dateFolder = string.Format("{0}\\{1}",
+                var dateFolder = string.Format("{0}\\{1}",
                     Constants.ReportsFolder,
                     DateTime.Now.ToShortDateString().Replace('-', '_'));
-                string destinationFolder = string.Format("{0}\\{1}",
+                var destinationFolder = string.Format("{0}\\{1}",
                         dateFolder,
                         DateTime.Now.ToLongTimeString().Replace(':', '_'));
                 if (!Directory.Exists(dateFolder))
@@ -54,16 +58,14 @@
 
                 Directory.CreateDirectory(destinationFolder);
 
-                foreach (var report in Reports)
+                foreach (var report in ReportList)
                 {
                     string destination = string.Format("{0}\\{1}.json",
                         destinationFolder,
-                        report["product-id"]);
-                    File.WriteAllText(@destination, report.ToJson(
-                        new JsonWriterSettings
-                        {
-                            OutputMode = JsonOutputMode.Strict
-                        }));
+                        report.ProductId);
+                    JsonConvert.SerializeObject(report, Formatting.Indented);
+                    File.WriteAllText(@destination,
+                        JsonConvert.SerializeObject(report, Formatting.Indented));
                 }
 
                 return true;
@@ -103,17 +105,27 @@
                         totalQuantity += sale.Quantity;
                     }
                 }
+                var report = new Report
+                {
+                    ProductId = product.ID,
+                    ProductName = product.Product_Name,
+                    TotalIncomes = totalIncomes,
+                    TotalQuantitySold = totalQuantity,
+                    VendorName = product.Vendor
+                };
 
-                var report = new BsonDocument
-                    { 
-                        {"product-id", product.ID},
-                        {"product-name", product.Product_Name},
-                        {"vendor-name", product.Vendor},
-                        {"total-quantity-sold", totalQuantity},
-                        {"total-incomes", Convert.ToDouble(totalIncomes)}
-                    };
 
-                Reports.Add(report);
+
+                //                var report = new BsonDocument
+                //                    { 
+                //                        {"product-id", product.ID},
+                //                        {"product-name", product.Product_Name},
+                //                        {"vendor-name", product.Vendor},
+                //                        {"total-quantity-sold", totalQuantity},
+                //                        {"total-incomes", Convert.ToDouble(totalIncomes)}
+                //                    };
+                ReportList.Add(report);
+                //                Reports.Add(report.ToBson());
             }
         }
 
@@ -170,26 +182,31 @@
                     db.CreateCollection(collectionName);
                 }
 
-                if (!Reports.Any())
+                if (!ReportList.Any())
                 {
                     GetReportsData();
                 }
 
-                foreach (var report in Reports)
+                foreach (var report in ReportList)
                 {
-                    var collection = db.GetCollection(collectionName);
-                    var currentReport = collection.FindOne(Query.EQ("product-id", report["product-id"]));
+                    //                    Query<Report>.Where(r=>r.ProductId == report.ProductId))
+                    var collection = db.GetCollection<Report>(collectionName);
+                    var currentReport = collection.FindOne(Query<Report>.Where(r => r.ProductId == report.ProductId));
                     if (currentReport == null)
                     {
                         collection.Insert(report);
                     }
                     else
                     {
+                        currentReport.ProductName = report.ProductName;
+                        currentReport.VendorName = report.VendorName;
+                        currentReport.TotalIncomes = report.TotalIncomes;
+                        currentReport.TotalQuantitySold = report.TotalQuantitySold;
                         // Set only new values without make new document
-                        currentReport["product-name"] = report["product-name"];
-                        currentReport["vendor-name"] = report["vendor-name"];
-                        currentReport["total-quantity-sold"] = report["total-quantity-sold"];
-                        currentReport["total-incomes"] = report["total-incomes"];
+                        //                        currentReport["product-name"] = report["product-name"];
+                        //                        currentReport["vendor-name"] = report["vendor-name"];
+                        //                        currentReport["total-quantity-sold"] = report["total-quantity-sold"];
+                        //                        currentReport["total-incomes"] = report["total-incomes"];
                         collection.Save(currentReport);
                     }
                 }
