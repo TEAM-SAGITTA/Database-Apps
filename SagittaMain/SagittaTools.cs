924083289c648ed28da1bb40e08296a47191c094
MeLean <milen820430@gmail.com>
2015-03-22 17:03:41 +0200

    SagittaTools.cs added

---
+++ SagittaMain/SagittaTools.cs
@@ -0,0 +1,33 @@
+﻿namespace SagittaMain
+{
+    using System.IO;
+
+    internal static class SagittaTools
+    {
+
+        internal static string FilePathPicker(string fileExtension)
+        {
+            Microsoft.Win32.OpenFileDialog filePicker = new Microsoft.Win32.OpenFileDialog();
+            if (fileExtension != "all")
+            {
+                filePicker.DefaultExt = fileExtension.ToString();
+                filePicker.Filter = string.Format("XML files (*{0})|*{0}", fileExtension);
+            }
+
+            filePicker.InitialDirectory = Directory.GetParent(
+                Directory.GetParent((
+                Directory.GetParent(
+                Directory.GetCurrentDirectory())).ToString()).ToString()).ToString() + "\\InputFiles";
+            bool? isFilePiced = filePicker.ShowDialog();
+            string filePath = filePicker.FileName;
+
+            return filePath;
+        }
+
+        internal static string FilePathPicker() 
+        {
+           return FilePathPicker("all");
+        }
+
+    }
+}
